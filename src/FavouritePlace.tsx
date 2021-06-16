be0d58b9f701745d419de0b134ae52f30aadafee
JWorthington97 <jb.worthington@live.co.uk>
2021-06-16 16:38:46 +0100

    Initial commit

---
+++ src/FavouritePlace.tsx
@@ -0,0 +1,27 @@
+interface PlaceInfoProps {
+    title: string
+    img_link: string
+    img_alt:string
+    location: string
+    map_link: string
+    body: string
+}
+
+function FavouritePlace(props: PlaceInfoProps): JSX.Element {
+    return <div style={{
+            background: "#85DCB0",
+            width: "1000px",
+            padding: "10px",
+            border: "10px",
+            margin: "0 auto",
+            marginTop: "30px",
+            borderRadius: "10px"
+            }}>
+        <img src={props.img_link} alt={props.img_alt} width="800px"></img>
+        <h2>{props.title}</h2>
+        <h3>{props.location} (<a href={props.map_link}>map link</a>)</h3>
+        <body>{props.body}</body>
+    </div>
+}
+
+export default FavouritePlace
---
+++ src/Header.tsx
@@ -0,0 +1,5 @@
+function Header(): JSX.Element {
+    return <h1>Coastlines</h1>
+}
+
+export default Header
